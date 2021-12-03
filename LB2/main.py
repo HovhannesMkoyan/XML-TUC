1b7830f0a6e4dadcbf4aaaaecf9419bde0ddf27f
Hovhannes <hovhannes.mkoyan@gmail.com>
2021-12-03 22:58:29 +0100

    parsing xml

--- LB2/main.py
+++ LB2/main.py
@@ -1,19 +1,17 @@
-# def fib(num):
-#     if num==0 or num==1 or num==2:
-#         return 1
-#     else:
-#         return fib(num - 1) + fib(num - 2)
+import xml.sax
+from xml.sax import handler
 
-# print(fib(20))
+class DeliveriesHandler(xml.sax.ContentHandler):
+    def startElement(self, name, attrs):
+        self.current = "----" + self.current
+        
+    def characters(self, content):
+        self.current = "----" + self.current
+        # print(content)
+        
 
-def fizz_buzz(input):
-    if input % 3 == 0 and input % 5 == 0:
-        print("FizzBuzz")
-    elif input % 3 == 0:
-        print("Fizz")
-    elif input % 5 == 0:
-        print("Buzz")
-    else:
-        print(input)
-
-fizz_buzz(7)
+handler = DeliveriesHandler()
+parser = xml.sax.make_parser()
+parser.setContentHandler(handler)
+parser.parse('/Users/hovhannesmkoyan/Desktop/XML/LB2/deliveries.xml')
+        
