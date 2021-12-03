3aa37055874e804b8d63dab689e94ab42f568f07
Hovhannes <hovhannes.mkoyan@gmail.com>
2021-12-03 23:42:11 +0100

    prettier

--- LB2/Task1/main.py
+++ LB2/Task1/main.py
@@ -1,46 +1,47 @@
 import xml.sax
-from xml.sax.handler import ContentHandler
 import sys
 
-
-class CompactHandler(ContentHandler):
+class CompactHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.closed = False
         self.indent = 0
         self.lastNodeWasText = False
 
-    def startElement(self, localname, attrs):
+    def startElement(self, tag, attrs):
         if self.closed:
             sys.stdout.write('\n'+self.indent*" ")
             self.closed = False
-        self.indent += 1+len(localname)
+        self.indent += 1+len(tag)
 
-        sys.stdout.write(localname+'[')
+        sys.stdout.write(tag+'[')
         first = True
+
         for name in attrs.getNames():
             if not first:
                 sys.stdout.write('\n'+self.indent*" ")
             first = False
             sys.stdout.write('@'+name+'['+attrs.getValue(name)+']')
             self.closed = True
+
         self.lastNodeWasText = False
 
-    def endElement(self, localname):
+    def endElement(self, tag):
         self.closed = True
         sys.stdout.write(']')
-        self.indent -= 1+len(localname)
+        self.indent -= 1+len(tag)
         self.lastNodeWasText = False
 
     def characters(self, data):
         data = data.strip()
+
         if(len(data) > 0):
             if self.closed and not self.lastNodeWasText:
                 sys.stdout.write('\n'+self.indent*" ")
                 self.closed = False
             sys.stdout.write(data)
             self.closed = True
-        self.lastNodeWasText = True
 
+        self.lastNodeWasText = True
 
 handler = CompactHandler()
 parser = xml.sax.make_parser()
