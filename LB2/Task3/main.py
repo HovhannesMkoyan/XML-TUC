b3eee798548e30c6d9c1e4ffce43b62e79de3257
Hovhannes <hovhannes.mkoyan@gmail.com>
2021-12-04 23:40:08 +0100

    solving t3

---
+++ LB2/Task3/main.py
@@ -0,0 +1,31 @@
+import xml.sax
+import sys
+
+articleIds = []
+
+class ConsistencyHandler(xml.sax.ContentHandler):
+    def startElement(self, tag, attrs):  # targets opening elements
+        if tag == 'article':
+            articleIds.append(attrs['id'])
+
+def detectDuplications():
+    for id in articleIds:
+        duplicatesNumber = 0
+        for possibleDuplicateId in articleIds:
+            if id == possibleDuplicateId:
+                duplicatesNumber += 1
+        print(f"Article ID {id} repeats {duplicatesNumber} times")
+        removeFromList(id)
+        
+    
+def removeFromList(id):
+    for value in articleIds:
+        if value == id:
+            articleIds.remove(id)
+
+        
+handler = ConsistencyHandler()
+parser = xml.sax.make_parser()
+parser.setContentHandler(handler)
+parser.parse('/Users/hovhannesmkoyan/Desktop/XML/LB2/deliveries.xml')
+detectDuplications()
