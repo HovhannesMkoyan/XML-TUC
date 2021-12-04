2d95bbded81270f39a30456a9a8a259386d9ac7a
Hovhannes <hovhannes.mkoyan@gmail.com>
2021-12-04 22:32:58 +0100

    solving t2

--- LB2/Task2/main.py
+++ LB2/Task2/main.py
@@ -1,22 +1,37 @@
 import xml.sax
-from xml.sax import handler
+import sys
 
-class AnythingHandler(xml.sax.ContentHandler):
+
+class ToHTMLHandler(xml.sax.ContentHandler):
     def __init__(self):
-        self.currentTag = ""
+        self.row = ""
 
-    def startElement(self, tag, attrs): # triggers when reading a tag
-        self.currentTag = tag
+    def startElement(self, tag, attrs):
+        if tag == 'article':
+            sys.stdout.write(f"<tr><td>{attrs['id']}</td>")
+        elif tag == 'name' or tag == 'price' or tag == 'supplier':
+            sys.stdout.write("<td>")
 
-    def characters(self, content):
+    def endElement(self, tag):
+        if tag == 'article':
+            sys.stdout.write("</tr>")
+            print(self.row)
+            self.row = ""
+        elif tag == 'name' or tag == 'price' or tag == 'supplier':
+            sys.stdout.write("</td>")
 
-    def endElement(args):
-        
-        
-        
+    def characters(self, data):
+        sys.stdout.write(data.strip())
 
-handler = AnythingHandler()
+
+handler = ToHTMLHandler()
 parser = xml.sax.make_parser()
 parser.setContentHandler(handler)
+
+print("<html><head><title>Deliveries</title></head>")
+print("<body><h1>Deliveries</h1><hr>")
+print("<table border=\"1\"><tr><th>Number</th><th>Article</th><th>Price</th><th>Supplier</th></tr>")
 parser.parse('/Users/hovhannesmkoyan/Desktop/XML/LB2/deliveries.xml')
-        
+print("</table>")
+print("</body>")
+print("</html>")
