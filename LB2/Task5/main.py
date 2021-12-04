b65c6995196b01bd22c2d754ef4e9176e20b9d6e
Hovhannes <hovhannes.mkoyan@gmail.com>
2021-12-04 13:17:33 +0100

    t5

--- LB2/Task5/main.py
+++ LB2/Task5/main.py
@@ -1,29 +1,21 @@
 import xml.dom.minidom
 
+dictionary = {}
+
+def transform(articles):
+    for article in articles:
+        supplier = article.getElementsByTagName('supplier')[0].childNodes[0].nodeValue
+        if supplier not in dictionary:
+            dictionary[supplier] = []
+        dictionary[supplier].append(article.getElementsByTagName('name')[0].childNodes[0].nodeValue)
+            
+def printSuppliersWithProducts():
+    print(dictionary)
+
+
 domtree = xml.dom.minidom.parse("/Users/hovhannesmkoyan/Desktop/XML/LB2/Task5/deliveries.xml")
 group = domtree.documentElement # this is the top element
 articles = group.getElementsByTagName('article')
 
-resultDictionary = {}
-
-def manipulate():
-    for article in articles:
-        supplier = article.getElementsByTagName('supplier')[0].childNodes[0].nodeValue
-        resultDictionary[supplier] = []
-        resultDictionary[supplier].append(article.getElementsByTagName('name')[0].childNodes[0].nodeValue)
-        
-        # supplierProduct = article.getElementsByTagName('name')[0].childNodes[0].nodeValue
-        # outputText = f"{supplier} supplies: {supplierProduct}"
-        # for iarticle in articles:
-        #     isupplier = iarticle.getElementsByTagName('supplier')[0].childNodes[0].nodeValue
-        #     isupplierProduct = iarticle.getElementsByTagName('name')[0].childNodes[0].nodeValue
-        #     if supplier == isupplier and supplierProduct != isupplierProduct:
-        #         outputText += " " + isupplierProduct
-            
-        
-def printResult():
-    manipulate()
-    print(resultDictionary)
-
-
-printResult()
+transform(articles)
+printSuppliersWithProducts()
