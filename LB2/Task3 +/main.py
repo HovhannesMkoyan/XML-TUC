fc2ff8e575443357d845af5838d5476cdea7ce52
Hovhannes <hovhannes.mkoyan@gmail.com>
2021-12-05 10:22:59 +0100

    adjusting t3

--- LB2/Task3 +/main.py
+++ LB2/Task3 +/main.py
@@ -13,7 +13,7 @@ def detectDuplications():
         for possibleDuplicateId in articleIds:
             if id == possibleDuplicateId:
                 duplicatesNumber += 1
-        if duplicatesNumber != 1:
+        if duplicatesNumber > 1:
             print(f"Article ID {id} repeats {duplicatesNumber} times")
         removeFromList(id)
 
